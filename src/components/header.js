b1ea6914ff0c0e80fb4baffe0d3c478474af6073
rahuldkjain <rahuldkjain@gmail.com>
2020-07-15 17:16:39 +0530

    header reload

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,14 @@ const Header = (props) => {
   return (
     <div className="header">
       <h1 className="heading">
-        <Link to="/" style={{textDecoration: 'none', color: '#0a0a23'}}>
+        <div onClick={() => window.location.reload(false)}>
           {props.heading}
-        </Link>
+        </div>
       </h1>
       <GitHubButton id="star-github" href="https://github.com/rahuldkjain/github-profile-readme-generator"
         data-icon="octicon-star" aria-label="Star rahuldkjain/github-profile-readme-generator on GitHub">
         Star this repo</GitHubButton>
+
       <GitHubButton id="fork-github" href="https://github.com/rahuldkjain/github-profile-readme-generator/fork"
         data-icon="octicon-repo-forked"
         aria-label="Fork rahuldkjain/github-profile-readme-generator on GitHub">Fork on Github</GitHubButton>
